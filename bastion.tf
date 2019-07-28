286672a3e21f128ca590b1225c9d45e9b786086c
brettinternet <brettinternet@gmail.com>
2019-07-28 02:55:07 -0600

    create terraform bastion server setup with digital oceans

---
+++ bastion.tf
@@ -0,0 +1,73 @@
+#### Variables ####
+
+variable "do_token" {}
+variable "pub_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "pvt_key" {
+  default = "~/.ssh/id_rsa"
+}
+variable "wireguard_port" {
+  default = "5555"
+}
+variable "wireguard_client_pub_key" {
+  default = ""
+}
+
+#### Digital Ocean ####
+
+provider "digitalocean" {
+  token = "${ var.do_token }"
+}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "Homelab Server (managed via Terraform)"
+  public_key = "${file(var.pub_key)}"
+}
+
+#### Setup ####
+
+resource "digitalocean_droplet" "bastion" {
+  image = "ubuntu-18-04-x64"
+  name = "bastion"
+  region = "sfo2"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    "${digitalocean_ssh_key.default.fingerprint}"
+  ]
+
+  connection {
+    host = "${digitalocean_droplet.bastion.ipv4_address}"
+    user = "root"
+    type = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    timeout = "1m"
+  }
+
+  provisioner "file" {
+    source      = "scripts/bastion_setup.sh"
+    destination = "/tmp/bastion_setup.sh"
+  }
+
+  provisioner "file" {
+    source      = "scripts/wireguard_install.sh"
+    destination = "/tmp/wireguard_install.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bastion_setup.sh",
+      "/tmp/bastion_setup.sh ${var.wireguard_port} ${var.wireguard_client_pub_key}",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = <<CMD
+      scp \
+        -o StrictHostKeyChecking=no \
+        -o UserKnownHostsFile=/dev/null \
+        -i ${var.pvt_key} \
+        root@${digitalocean_droplet.bastion.ipv4_address}:/tmp/wg0.conf.client .
+    CMD
+  }
+}
