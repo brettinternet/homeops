0fb329c2260d491d4c231e3b9253e173ced5687c
brettinternet <brettinternet@gmail.com>
2019-08-31 09:49:57 -0600

    add test docker-compose

--- scripts/wireguard/postdown.sh
+++ scripts/wireguard/postdown.sh
@@ -33,7 +33,8 @@ do
   iptables -t nat -D PREROUTING -p tcp --dport $KEY -j DNAT --to-destination $CLIENT_WG_IPV4:${PORTS_TO_FORWARD[$KEY]}
   [[ -n "$CLIENT_WG_IPV6" ]] && ip6tables -t nat -D PREROUTING -p tcp --dport $KEY -j DNAT --to-destination $CLIENT_WG_IPV6:${PORTS_TO_FORWARD[$KEY]}
 
-  # SNAT
-  iptables -t nat -D POSTROUTING -p tcp -o $SERVER_WG_NIC -j SNAT --to-source $SERVER_WG_IPV4
-  [[ -n "$SERVER_WG_IPV6" ]] && ip6tables -t nat -D POSTROUTING -p tcp -o $SERVER_WG_NIC -j SNAT --to-source $SERVER_WG_IPV6
 done
+
+# SNAT
+iptables -t nat -D POSTROUTING -p tcp -o $SERVER_WG_NIC -j SNAT --to-source $SERVER_WG_IPV4
+[[ -n "$SERVER_WG_IPV6" ]] && ip6tables -t nat -D POSTROUTING -p tcp -o $SERVER_WG_NIC -j SNAT --to-source $SERVER_WG_IPV6
