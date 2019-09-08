5023a1da589ef2ac1775f1d553fa8a3399b4f72a
brettinternet <brettinternet@gmail.com>
2019-09-08 11:56:36 -0600

    reorder array so traefik and auth are first to start

--- up.sh
+++ up.sh
@@ -4,11 +4,13 @@ CONTAINERS="$@"
 
 # if no arguments, start all services
 if [ -z "$CONTAINERS" ]; then
-  CONTAINERS=$(ls compose -1 | sed -e 's/\..*$//')
+  CONTAINERS=$(ls compose -I "traefik*" -I "auth*" -1 | sed -e 's/\..*$//')
+  CONTAINERS=('traefik' 'auth' "${CONTAINERS[@]}")
 fi
 
 # start each service from yml file in docker-compose detached mode
-for c in $CONTAINERS; do
+for c in ${CONTAINERS[@]}; do
   printf "\n### $c up ###\n"
   docker-compose -f "compose/$c.yml" -p $c up -d
+  printf "\n"
 done
