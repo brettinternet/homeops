34fb8755203604d6861f2878addb35e1da0c7fa6
brett <brettinternet@gmail.com>
2021-01-22 21:16:52 -0700

    remove old ansible dir

--- ansible/roles/terraform/files/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "do_token" {
-  description = "Digital Ocean token (https://www.digitalocean.com/docs/apis-clis/api/create-personal-access-token/)"
-}
-
-variable "openssh_keypair_path" {
-  default = "~/.ssh/id_rsa"
-  description = "Directory containing the public and private key. The file containing the public key will have the extension .pub."
-}
