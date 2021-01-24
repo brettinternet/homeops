de67ff37be007a2d49e421f11a49ac4cfb602a7d
brett <brettinternet@gmail.com>
2021-01-23 21:18:30 -0700

    change default droplet provision to debian 10, copy ubuntu task for debian in upgrade role

--- roles/terraform/files/variables.tf
+++ roles/terraform/files/variables.tf
@@ -10,3 +10,12 @@ variable "openssh_keypair_path" {
 variable "ssh_key_name" {
   default = "Homelab Server"
 }
+
+# Get image slug by selecting a distribution on https://cloud.digitalocean.com/droplets/new
+variable "droplet_image" {
+  default = "debian-10-x64"
+}
+
+variable "droplet_region" {
+  default = "sfo2"
+}
