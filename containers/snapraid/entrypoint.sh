524ac27c4ec4203fea495832074a77e67f3241ed
brett <brettinternet@gmail.com>
2022-11-02 15:54:54 -0600

    add pre and post commands

--- containers/snapraid/entrypoint.sh
+++ containers/snapraid/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/ash
 
+### Validation and Setup
+
 # remove default configuration
 if [ ! -L /etc/snapraid.conf ] && [ -f /etc/snapraid.conf ]; then
     rm /etc/snapraid.conf
@@ -22,4 +24,46 @@ if [ ! -L /etc/snapraid.conf ]; then
     ln -s /config/snapraid.conf /etc/snapraid.conf
 fi
 
+### Declarations
+
+function run_commands {
+  COMMANDS=$1
+  while IFS= read -r cmd; do echo "$cmd" && eval "$cmd" ; done < <(printf '%s\n' "$COMMANDS")
+}
+
+function run_exit_commands {
+  set +e
+  set +o pipefail
+  run_commands "${POST_COMMANDS_EXIT:-}"
+}
+
+### Runtime
+
+trap run_exit_commands EXIT
+
+run_commands "${PRE_COMMANDS:-}"
+
+start=$(date +%s)
+echo Starting SnapRAID runner at $(date +"%Y-%m-%d %H:%M:%S")
+
+set +e
 /usr/bin/python3 /app/snapraid-runner/snapraid-runner.py -c /config/snapraid-runner.conf
+RC=$?
+set -e
+
+if [ $RC -ne 0 ]; then
+  if [ $RC -eq 3 ] && [ -n "${POST_COMMANDS_INCOMPLETE:-}" ]; then
+      run_commands "${POST_COMMANDS_INCOMPLETE:-}"
+  else
+      run_commands "${POST_COMMANDS_FAILURE:-}"
+  fi
+fi
+
+echo Runner successful
+
+end=$(date +%s)
+echo Finished SnapRAID runner at $(date +"%Y-%m-%d %H:%M:%S") after $((end-start)) seconds
+
+[ $rc -ne 0 ] && exit $rc
+
+run_commands "${POST_COMMANDS_SUCCESS:-}"
