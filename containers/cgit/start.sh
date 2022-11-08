d704a44ca37801893c1af90765aaa56986269169
brett <brettinternet@gmail.com>
2022-11-08 11:56:59 -0700

    fix cgit setup

--- containers/cgit/start.sh
+++ containers/cgit/start.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-CGIT_VARS='$ROOT_TITLE:$ROOT_DESC:$SECTION_FROM_STARTPATH:$MAX_REPO_COUNT'
+CGIT_VARS='$ROOT_TITLE:$ROOT_DESC:$SECTION_FROM_STARTPATH:$MAX_REPO_COUNT:$ROOT_README'
 envsubst "$CGIT_VARS" < /etc/cgitrc.template > /etc/cgitrc
 
-# Number of fcgi workers
-if [ -z "$FCGI_CHILDREN" ]; then
-  FCGI_CHILDREN=$(nproc)
-fi
-
-spawn-fcgi -F $FCGI_CHILDREN -U nginx -G nginx -M 600 -s /var/run/fcgiwrap.sock /usr/bin/fcgiwrap
+spawn-fcgi -U nginx -G nginx -M 600 -s /var/run/fcgiwrap.sock /usr/bin/fcgiwrap
 
 nginx -g "daemon off;"
