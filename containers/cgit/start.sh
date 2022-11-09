d67276deca0a3d79e7f1740bffad25c059c06815
brett <brettinternet@gmail.com>
2022-11-08 23:19:10 -0700

    fix cgit highlighting, add options

--- containers/cgit/start.sh
+++ containers/cgit/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CGIT_VARS='$ROOT_TITLE:$ROOT_DESC:$SECTION_FROM_STARTPATH:$MAX_REPO_COUNT:$ROOT_README'
+CGIT_VARS='$ROOT_TITLE:$ROOT_DESC:$SECTION_FROM_STARTPATH:$MAX_REPO_COUNT:$ROOT_README:$NOPLAINEMAIL'
 envsubst "$CGIT_VARS" < /etc/cgitrc.template > /etc/cgitrc
 
 spawn-fcgi -U nginx -G nginx -M 600 -s /var/run/fcgiwrap.sock /usr/bin/fcgiwrap
