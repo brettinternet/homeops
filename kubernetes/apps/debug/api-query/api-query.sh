6af725f30352992f386990ba21414cc3aef32c9f
brett <brettinternet@gmail.com>
2023-12-12 00:16:11 -0700

    convert services back to docker-compose, 🪦 kubernetes

--- kubernetes/apps/debug/api-query/api-query.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-API_SERVER="https://kubernetes.default.svc"
-SERVICE_ACCOUNT_FOLDER="/var/run/secrets/kubernetes.io/serviceaccount"
-NAMESPACE=$(cat ${SERVICE_ACCOUNT_FOLDER}/namespace)
-TOKEN=$(cat ${SERVICE_ACCOUNT_FOLDER}/token)
-CACERT="${SERVICE_ACCOUNT_FOLDER}/ca.crt"
-
-curl -s --cacert "$CACERT" --header "Authorization: Bearer $TOKEN" -X GET "$API_SERVER/apis/apps/v1/namespaces/$NAMESPACE/pods"
