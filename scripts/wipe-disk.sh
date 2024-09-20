52e643583fd34c641a8027e69217be12ee67e564
brett <brettinternet@gmail.com>
2024-09-20 14:16:38 -0600

    add helper

---
+++ scripts/wipe-disk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Zap device disk!
+# https://rook.io/docs/rook/latest-release/Getting-Started/ceph-teardown/#zapping-devices
+
+# Run this in the rook ceph tools pod container on each node:
+
+DISK="/dev/nvme1n1"; \
+  sgdisk --zap-all $DISK; \
+  dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync; \
+  blkdiscard $DISK; \
+  partprobe $DISK
+
+# Then, delete the operator pod to re-run the osd-prepare job pods
