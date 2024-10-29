568e0413c9c4fc8685889ce76e18bddf90e782c7
brett <brettinternet@gmail.com>
2024-10-29 17:28:37 -0600

    --wip-- [skipci]

--- scripts/wipe-disk.sh
+++ scripts/wipe-disk.sh
@@ -5,10 +5,17 @@
 
 # Run this in the rook ceph tools pod container on each node:
 
-DISK="/dev/sda"; \
+# One liner for the rook ceph tools pod container:
+# You may also need to remove LVM and device mapper data:
+FDISK_BEFORE=$(fdisk -l); \
+  DISK="/dev/sda"; \
   sgdisk --zap-all $DISK; \
   dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync; \
   blkdiscard $DISK; \
-  partprobe $DISK
+  partprobe $DISK; \
+  ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %; \
+  rm -rf /dev/ceph-*; \
+  rm -rf /dev/mapper/ceph--*; \
+  diff <(echo "$FDISK_BEFORE") <(fdisk -l)
 
 # Then, delete the operator pod to re-run the osd-prepare job pods
