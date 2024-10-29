8b45fe640854642be4d710662c07008b11280863
brett <brettinternet@gmail.com>
2024-10-29 12:11:23 -0600

    --wip-- [skipci]

--- scripts/wipe-disk.sh
+++ scripts/wipe-disk.sh
@@ -5,7 +5,7 @@
 
 # Run this in the rook ceph tools pod container on each node:
 
-DISK="/dev/nvme1n1"; \
+DISK="/dev/sda"; \
   sgdisk --zap-all $DISK; \
   dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync; \
   blkdiscard $DISK; \
